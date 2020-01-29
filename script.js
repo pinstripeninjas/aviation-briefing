dd460f50c4c639bca8251ac282ee4638807fc06a
pinstripeninjas <pinstripeninjas@gmail.com>
2020-01-29 15:48:00 -0700

    html template set, ready to write JS

---
+++ script.js
@@ -0,0 +1,13 @@
+const leftContent = document.querySelector("#left-content");
+const rightContent = document.querySelector("#right-content");
+
+//----------------- Starting Content ---------------------------//
+//buildTableBody();
+//buildTableHeader();
+//buildCriteriaBody();
+//buildCriteriaHeader();
+
+function clearContent() {
+	leftContent.innerHTML = "";
+	rightContent.innerHTML = "";
+}
